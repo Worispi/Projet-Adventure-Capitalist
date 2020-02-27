6a0fe52ee855b6a0dc694a4ab52c0b07eda668cb
bastien Berruezo <bastien.berruezo@gmail.com>
2020-02-27 09:01:43 +0100

    Initial Commit

---
+++ src/test/java/com/isis/adventureISIServer/demo/IsisCapitalistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isis.adventureISIServer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsisCapitalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
